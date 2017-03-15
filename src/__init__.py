cac559068a2997db731e02c377eb9913d2e78641
Roberto Lotufo <robertoalotufo@gmail.com>
2017-03-15 17:57:57 -0300

    varios

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,6 @@
 #
 
 __version__ = 'ia898 Python Toolbox for Teaching Image  Processing version 0.1 12Mar2017'
-from nbpil import show, nbshow, nbread, nbreadgray, nbwrite
-from normalize import normalize
+from ia898.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
+from ia898.src.normalize import normalize
 
