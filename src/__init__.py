199aaaf0c91d318c12bf7632ad9444826e1bd9b6
Roberto Lotufo <robertoalotufo@gmail.com>
2017-03-15 12:42:56 -0300

    normalize

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,5 @@
 
 __version__ = 'ia898 Python Toolbox for Teaching Image  Processing version 0.1 12Mar2017'
 from nbpil import show, nbshow, nbread, nbreadgray, nbwrite
+from normalize import normalize
+
