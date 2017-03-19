e522a1d8e2e8d0b1b47b071bfa34409b6d44abf0
Roberto A Lotufo <robertoalotufo@github.com>
2017-03-19 20:58:32 -0300

    h2stats

--- src/__init__.py
+++ src/__init__.py
@@ -6,4 +6,6 @@ __version__ = 'ia898 Python Toolbox for Teaching Image  Processing version 0.1 1
 from ia898.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
 from ia898.src.normalize import normalize
 from ia898.src.histogram import histogram, histogram_eq, histogram_eq1
+from ia898.src.h2percentile import h2percentile
+from ia898.src.h2stats import h2stats
 
