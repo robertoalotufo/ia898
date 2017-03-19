0ec314aac931ef06576629a1a1b3d2dd27d91dc1
Roberto A Lotufo <robertoalotufo@gmail.com>
2017-03-19 20:24:58 -0300

    histogram

--- src/__init__.py
+++ src/__init__.py
@@ -5,4 +5,5 @@
 __version__ = 'ia898 Python Toolbox for Teaching Image  Processing version 0.1 12Mar2017'
 from ia898.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
 from ia898.src.normalize import normalize
+from ia898.src.histogram import histogram, histogram_eq, histogram_eq1
 
