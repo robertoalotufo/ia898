705e4fa6cd91db522aafd216a110c9744d80f569
Roberto Lotufo <robertoalotufo@gmail.com>
2017-07-02 17:16:10 -0300

    mais

--- src/__init__.py
+++ src/__init__.py
@@ -3,22 +3,23 @@
 #
 
 __version__ = 'ia898 Python Toolbox for Teaching Image  Processing version 0.1 12Mar2017'
+from ia898.src.affine import affine
+from ia898.src.applylut import applylut
+from ia898.src.colormap import colormap
+from ia898.src.conv import conv
+from ia898.src.comb import comb
+from ia898.src.circle import circle
+from ia898.src.dft import dft
+from ia898.src.dftmatrix import dftmatrix
+from ia898.src.dftshift import dftshift
+from ia898.src.dftview import dftview
 from ia898.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
 from ia898.src.normalize import normalize
 from ia898.src.histogram import histogram, histogram_eq, histogram_eq1
 from ia898.src.h2percentile import h2percentile
 from ia898.src.h2stats import h2stats
-from ia898.src.applylut import applylut
-from ia898.src.colormap import colormap
-from ia898.src.conv import conv
-from ia898.src.affine import affine
 from ia898.src.ptrans import ptrans, ptrans2, ptrans2d
 from ia898.src.pconv import pconv
-from ia898.src.dftmatrix import dftmatrix
-from ia898.src.dftshift import dftshift
-from ia898.src.dftview import dftview
-from ia898.src.dft import dft
-from ia898.src.comb import comb
 from ia898.src.isccsym import isccsym,isccsym2
 from ia898.src.polar import polar
 from ia898.src.interpollin import interpollin
