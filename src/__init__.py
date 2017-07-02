0e8c0f259104b119732b36e36290642e426b4b9b
Roberto Lotufo <robertoalotufo@gmail.com>
2017-07-02 20:27:15 -0300

    mais

--- src/__init__.py
+++ src/__init__.py
@@ -5,14 +5,27 @@
 __version__ = 'ia898 Python Toolbox for Teaching Image  Processing version 0.1 12Mar2017'
 from ia898.src.affine import affine
 from ia898.src.applylut import applylut
+from ia898.src.bwlp import bwlp
 from ia898.src.colormap import colormap
 from ia898.src.conv import conv
 from ia898.src.comb import comb
 from ia898.src.circle import circle
+from ia898.src.dct import dct
+from ia898.src.dctmatrix import dctmatrix
 from ia898.src.dft import dft
 from ia898.src.dftmatrix import dftmatrix
 from ia898.src.dftshift import dftshift
 from ia898.src.dftview import dftview
+from ia898.src.ellipse import ellipse
+from ia898.src.gaussian import gaussian
+from ia898.src.haarmatrix import haarmatrix
+from ia898.src.hadamardmatrix import hadamardmatrix
+from ia898.src.hadamard import hadamard
+from ia898.src.idft import idft
+from ia898.src.ihadamard import ihadamard
+from ia898.src.isolines import isolines
+from ia898.src.log import log
+from ia898.src.logfilter import logfilter
 from ia898.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
 from ia898.src.normalize import normalize
 from ia898.src.histogram import histogram, histogram_eq, histogram_eq1
