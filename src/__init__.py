b3c83913e6dff081fc727107842af9565e9ddd67
Roberto Lotufo <robertoalotufo@gmail.com>
2017-07-02 22:48:46 -0300

    finalizando

--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,7 @@ from ia898.src.bwlp import bwlp
 from ia898.src.colormap import colormap
 from ia898.src.conv import conv
 from ia898.src.comb import comb
+from ia898.src.cos import cos
 from ia898.src.circle import circle
 from ia898.src.dct import dct
 from ia898.src.dctmatrix import dctmatrix
@@ -22,25 +23,29 @@ from ia898.src.gshow import gshow
 from ia898.src.haarmatrix import haarmatrix
 from ia898.src.hadamardmatrix import hadamardmatrix
 from ia898.src.hadamard import hadamard
+from ia898.src.histogram import histogram, histogram_eq, histogram_eq1
+from ia898.src.h2percentile import h2percentile
+from ia898.src.h2stats import h2stats
+from ia898.src.idct import idct
 from ia898.src.idft import idft
 from ia898.src.ihadamard import ihadamard
+from ia898.src.interpollin import interpollin
+from ia898.src.isccsym import isccsym,isccsym2
 from ia898.src.isolines import isolines
 from ia898.src.log import log
 from ia898.src.logfilter import logfilter
 from ia898.src.mosaic import mosaic
 from ia898.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
 from ia898.src.normalize import normalize
-from ia898.src.histogram import histogram, histogram_eq, histogram_eq1
-from ia898.src.h2percentile import h2percentile
-from ia898.src.h2stats import h2stats
-from ia898.src.ptrans import ptrans, ptrans2, ptrans2d
+from ia898.src.pca import pca
 from ia898.src.pconv import pconv
-from ia898.src.isccsym import isccsym,isccsym2
+from ia898.src.phasecorr import phasecorr
+from ia898.src.ptrans import ptrans, ptrans2, ptrans2d
 from ia898.src.polar import polar
 from ia898.src.ramp import ramp
 from ia898.src.rectangle import rectangle
-from ia898.src.interpollin import interpollin
-from ia898.src.phasecorr import phasecorr
+from ia898.src.sat import sat, satarea
+from ia898.src.sobel import sobel
 
 
 
